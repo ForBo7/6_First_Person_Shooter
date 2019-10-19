bbee6cf38ab99ea5d95f43df6ac4e47434fb8068
Salman Naqvi <salmananaqvii@gmail.com>
2019-10-19 10:21:39 +0500

    Added raycast to the gun. Gun doesn't shoot yet.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // Why on planet earth does this commented out code give errors??????
+
+    //[SerializeField] Camera FPCamera;
+    //[SerializeField] float range;
+
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        Shoot();
+    //    }
+    //}
+
+    //private static void Shoot()
+    //{
+    //    RaycastHit hit;
+    //    Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+    //    print("Raycast has hit " + hit.transform.name);
+    //}
+
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range;
+    Animation gunAnimation;
+
+    private void Start()
+    {
+        gunAnimation = GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+        Debug.Log("Raycast has hit " + hit.transform.name);
+    }
+
+}
