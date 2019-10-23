59d6702988f98df81712e6eb2ebafac9a2f23876
Salman Naqvi <salmananaqvii@gmail.com>
2019-10-23 21:29:40 +0500

    Added an attack animation event.

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float damage;
+
+    public void AttackHitEvent()
+    {
+        if (target == null)
+        {
+            return; // to protect against null
+        }
+        Debug.Log("Boop!");
+    }
+
+}
