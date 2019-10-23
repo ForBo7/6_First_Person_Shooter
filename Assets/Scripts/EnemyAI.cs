a3fe649166b46b630f2ae9dd652c979fa75c0010
Salman Naqvi <salmananaqvii@gmail.com>
2019-10-23 21:19:08 +0500

    Animations of the enemy now trigger via code.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -30,9 +30,6 @@ public class EnemyAI : MonoBehaviour
         {
             isProvoked = true;
         }
-
-
-
     }
 
     private void EngageTarget()
@@ -50,11 +47,14 @@ public class EnemyAI : MonoBehaviour
 
     private void AttackTarget()
     {
+        GetComponent<Animator>().SetBool("Attack", true);
         Debug.Log(name + " is attacking " + target.name);
     }
 
     private void ChaseTarget()
     {
+        GetComponent<Animator>().SetBool("Attack", false);
+        GetComponent<Animator>().SetTrigger("Move");
         navMeshAgent.SetDestination(target.position);
     }
 
