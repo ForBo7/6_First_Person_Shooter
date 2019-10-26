bfe6f1124c62f738fd8b99c1dbb75a7ef51ba166
Salman Naqvi <salmananaqvii@gmail.com>
2019-10-26 10:35:27 +0500

    Added weapon zoom.

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] float zoomedInFOV;
+    [SerializeField] float zoomedOutFOV;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            ZoomFOV();
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            ZoomOutFOV();
+        }
+    }
+
+    private void ZoomFOV()
+    {
+        Camera.main.fieldOfView = zoomedInFOV;
+    }
+
+    private void ZoomOutFOV()
+    {
+        Camera.main.fieldOfView = zoomedOutFOV;
+    }
+}
