ad7412b541c232e514cf2e5f71f31b26b6c00c54
Salman Naqvi <salmananaqvii@gmail.com>
2019-10-27 21:57:16 +0500

    Added basic set active weapon functionality.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,26 +5,6 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Why on planet earth does this commented out code give errors??????
-
-    //[SerializeField] Camera FPCamera;
-    //[SerializeField] float range;
-
-    //void Update()
-    //{
-    //    if (Input.GetMouseButtonDown(0))
-    //    {
-    //        Shoot();
-    //    }
-    //}
-
-    //private static void Shoot()
-    //{
-    //    RaycastHit hit;
-    //    Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-    //    print("Raycast has hit " + hit.transform.name);
-    //}
-
     [SerializeField] Camera FPCamera;
     [SerializeField] float range;
     [SerializeField] float gunDamage;
