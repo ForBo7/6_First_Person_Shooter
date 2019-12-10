04b3f2e08c8e619d55bacc7bc563a4d28d25e267
Salman Naqvi <salmananaqvii@gmail.com>
2019-12-10 10:17:56 +0500

    Gave the player a flashlight and created a battery pickup for the flash light.

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+	[SerializeField] float lightFade = 0f;
+	[SerializeField] float angleFade = 0f;
+    [SerializeField] float minimumAngleFade = 0f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightIntensity();
+        DecreaseLightAngle();
+    }
+
+    public void RestoreLightAngle(float restoreAngle)
+    {
+        myLight.spotAngle = restoreAngle;
+    }
+
+    public void RestoreLightIntensity(float restoreIntensity)
+    {
+        myLight.intensity = restoreIntensity;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle <= minimumAngleFade)
+        {
+            return;
+        }
+        else
+        {
+            myLight.spotAngle -= angleFade * Time.deltaTime;
+        }
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        if (myLight.intensity <= 0)
+        {
+            return;
+        }
+        else
+        {
+            myLight.intensity -= lightFade * Time.deltaTime;
+        }
+    }
+}
