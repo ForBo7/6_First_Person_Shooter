c78d380c142d1b168e4238ed2415a1abf1c094cc
Salman Naqvi <salmananaqvii@gmail.com>
2019-12-11 10:49:37 +0500

    Added blood VFX

---
+++ Assets/DisplayBloodSplatter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayBloodSplatter : MonoBehaviour
+{
+	[SerializeField] Canvas bloodVFXCanvas;
+	[SerializeField] float impactTime;
+
+	private void Start()
+	{
+		bloodVFXCanvas.enabled = false;
+	}
+
+	public void ShowBloodVFX()
+	{
+		StartCoroutine(ShowBloodSplatter());
+	}
+
+	IEnumerator ShowBloodSplatter()
+	{
+		bloodVFXCanvas.enabled = true;
+		yield return new WaitForSeconds(impactTime);
+		bloodVFXCanvas.enabled = false;
+	}
+}
--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -17,5 +17,6 @@ public class EnemyAttack : MonoBehaviour
     {
         if (target == null) return; // To protect against null
         target.TakeDamage(damage);
+        target.GetComponent<DisplayBloodSplatter>().ShowBloodVFX();
     }
 }
