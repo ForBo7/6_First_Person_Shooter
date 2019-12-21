06599ca1274a7e2255f9932142dd93ce5f0fd43a
Salman Naqvi <salmananaqvii@gmail.com>
2019-12-21 21:44:52 +0500

    The flashlight can now be turned on and off.

--- Assets/Scripts/FlashLightSystem.cs
+++ Assets/Scripts/FlashLightSystem.cs
@@ -10,26 +10,36 @@ public class FlashLightSystem : MonoBehaviour
     [SerializeField] float minimumAngleFade = 0f;
 
     Light myLight;
+    float currentLightIntensity;
+    float currentLightAngle;
+    bool isFlashLightOn;
 
     private void Start()
     {
         myLight = GetComponent<Light>();
+        currentLightIntensity = myLight.intensity;
+        currentLightAngle = myLight.spotAngle;
+        myLight.intensity = 0f;
+        myLight.spotAngle = 0f;
     }
 
     private void Update()
     {
         DecreaseLightIntensity();
         DecreaseLightAngle();
+        TurnOnFlashLight();
     }
 
     public void RestoreLightAngle(float restoreAngle)
     {
         myLight.spotAngle = restoreAngle;
+        currentLightAngle = restoreAngle;
     }
 
     public void RestoreLightIntensity(float restoreIntensity)
     {
         myLight.intensity = restoreIntensity;
+        currentLightIntensity = restoreIntensity;
     }
 
     private void DecreaseLightAngle()
@@ -41,6 +51,7 @@ public class FlashLightSystem : MonoBehaviour
         else
         {
             myLight.spotAngle -= angleFade * Time.deltaTime;
+            currentLightAngle = myLight.spotAngle;
         }
     }
 
@@ -53,6 +64,24 @@ public class FlashLightSystem : MonoBehaviour
         else
         {
             myLight.intensity -= lightFade * Time.deltaTime;
+            currentLightIntensity = myLight.intensity;
         }
     }
+
+    private void TurnOnFlashLight() 
+    {
+        if(Input.GetKeyDown(KeyCode.F) && isFlashLightOn)
+        {
+            myLight.intensity = 0f;
+            myLight.spotAngle = 0f;
+            isFlashLightOn = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.F) && !isFlashLightOn) 
+        {
+            myLight.intensity = currentLightIntensity;
+            myLight.spotAngle = currentLightAngle;
+            isFlashLightOn = true;
+        }
+    }
+
 }
