d5c8437d0c9163133e8d618c0ae7972b44ad5a7d
Salman Naqvi <salmananaqvii@gmail.com>
2019-12-24 21:21:19 +0500

    Added pickups into the world and gave zombies attack SFX.

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     [SerializeField] float damage;
+    [SerializeField] AudioClip []attackSounds;
 
+    AudioSource audioSource;
     PlayerHealth target;
 
     private void Start()
     {
         target = FindObjectOfType<PlayerHealth>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void AttackHitEvent()
     {
         if (target == null) return; // To protect against null
+        if (Random.Range(0,2) == 1)
+        {
+            audioSource.PlayOneShot(attackSounds[1]);
+        }
+        else
+        {
+            audioSource.PlayOneShot(attackSounds[0]);
+        }
         target.TakeDamage(damage);
         target.GetComponent<DisplayBloodSplatter>().ShowBloodVFX();
     }
